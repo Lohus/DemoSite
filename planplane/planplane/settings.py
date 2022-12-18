7e2a4e0316c959ab17a63add5a1e914784a29e48
Lohus <alhov271@gmail.com>
2022-12-19 02:16:05 +0500

    connection with postgres in settings.py

--- planplane/planplane/settings.py
+++ planplane/planplane/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'planplane.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'demo',
+        'USER': 'djangouser',
+        'PASSWORD': 'djangopass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
